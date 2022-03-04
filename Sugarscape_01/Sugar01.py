b0aee5eea1314143384f4ec37dd91d0c75a8be98
= <=>
2022-03-04 13:22:58 +0000

    with working difusion

--- Sugarscape_01/Sugar01.py
+++ Sugarscape_01/Sugar01.py
@@ -9,7 +9,7 @@ from pyabm import World, Cell, Agent
 pygame.init()
  
 # Colours
-BACKGROUND = (255, 255, 255)
+BACKGROUND = (0, 0, 0)
 RED = (255, 30, 70)
 BLUE = (10, 20, 200)
 GREEN = (50, 230, 40)
@@ -23,11 +23,10 @@ WINDOW_HEIGHT = 600
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Sugarscape-01')
 
-def draw(screen,world,state,value,discreet=False):
+def draw(world,state,value,discreet=False):
     width = WINDOW_WIDTH
     ratio = width/world.size
     box_size = int(ratio-1)
-
     
     for cell in world.cells:
         x_pos = int(cell.x_pos*ratio)
@@ -53,6 +52,11 @@ def draw(screen,world,state,value,discreet=False):
 # The main function that controls the game
 def main () :
   looping = True
+
+  world = World(30, ["resource"],[0])
+  #set center cell resource to 500
+  world.setCell(15, 15, "resource", 500)
+  world.update()
   
   # The main game loop
   while looping :
@@ -63,13 +67,14 @@ def main () :
         sys.exit()
     
     # Processing
+    world.diffuse("resource",0.5)
+    world.update()
     
     # This section will be built out later
  
     # Render elements of the game
     WINDOW.fill(BACKGROUND)
-    draw()
-    pygame.draw.rect(WINDOW, RED, (rnd.randint(0,WINDOW_WIDTH),rnd.randint(0,WINDOW_HEIGHT), 10, 10))
+    draw( world, "resource", 1,discreet=False)
     pygame.display.update()
     fpsClock.tick(FPS)
  
